64e41f0bf4012b1ac1325a88fa6d3828608d8d91
Angie Bai <anqib48356@foxmail.com>
2014-10-15 17:05:13 -0400

    clean seg_Gui1.m file to start

--- seg_Gui1.m
+++ seg_Gui1.m
@@ -42,7 +42,7 @@ else
     gui_mainfcn(gui_State, varargin{:});
 end
 % End initialization code - DO NOT EDIT
-
+end
 
 % --- Executes just before seg_Gui1 is made visible.
 function seg_Gui1_OpeningFcn(hObject, eventdata, handles, varargin)
@@ -57,7 +57,7 @@ handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
-
+end
 % UIWAIT makes seg_Gui1 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
@@ -71,38 +71,85 @@ function varargout = seg_Gui1_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
-
+end
 
 % --- Executes on button press in load_images.
 function load_images_Callback(hObject, eventdata, handles)
 % hObject    handle to load_images (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+[filename, pathname,~]=uploadimage(hObject, eventdata, handles);
+files={};
 
+if isnumeric(filename)
+    return;
+else
+    if iscell(filename)
+        ImgNum=length(filename);
+        for i=1:ImgNum
+            files{i}=[pathname filename{i}];
+        end        
+    else
+        ImgNum=1;
+        files{1}=[pathname filename];
+    end
+end
+
+handles.ImgIdx=0;
+handles.ImgNum=ImgNum;
+handles.filename=files;
+handles.Seeds=cell(ImgNum,1);
+handles.Seeds_num=zeros(ImgNum,1);
+handles.marks=cell(ImgNum,1);
+
+set(handles.Current_Image_index,'string',num2str(handles.ImgIdx));
+set(handles.next_image,'Enable','Off');
+set(handles.current_image,'Enable','off');
+set(handles.previous_image,'Enable','off');
+set(handles.next_image,'String','First');
+set(handles.next_image,'Enable','on');
+%set(handles.text1,'String',['Processing Image ', num2str(handles.ImgIdx), '/', num2str(handles.ImgNum)]);
+guidata(hObject, handles);
+end
 
 % --- Executes on button press in start_segmentation.
 function start_segmentation_Callback(hObject, eventdata, handles)
 % hObject    handle to start_segmentation (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+end
 
 % --- Executes on button press in previous_image.
 function previous_image_Callback(hObject, eventdata, handles)
 % hObject    handle to previous_image (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+end
 
 % --- Executes on button press in current_image.
 function current_image_Callback(hObject, eventdata, handles)
 % hObject    handle to current_image (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+end
 
 % --- Executes on button press in next_image.
 function next_image_Callback(hObject, eventdata, handles)
 % hObject    handle to next_image (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+end
+
+function [filename, pathname,FilterIndex] =uploadimage(hObject, eventdata, handles)
+[filename, pathname,FilterIndex] = uigetfile( ...
+    {'*.png;*.jpg;*.jpeg;*.jpe;*.jfif;*.tif;*.tiff;*.bmp;*.dib;*.gif','Input Images(*.png,*.jpg,*.jpeg,*.tif,*.bmp,*.gif)';
+    '*.png',  'PNG (*.png)'; ...
+    '*.jpg;*.jpeg;*.jpe;*.jfif','JPEG (*.jpg,*.jpeg,*.jpe,*.jfif)'; ...
+    '*.tif;*.tiff','TIFF (*.tif,*.tiff)'; ...
+    '*.bmp;*.dib','Bitmap Files (*.bmp,*.dib)'; ...
+    '*.gif','GIF(*.gif)'; ...
+    '*.*',  'All Files (*.*)'}, ...
+    'MultiSelect', 'on', ...
+    'Choose Training Images');
+guidata(hObject, handles);
+end
